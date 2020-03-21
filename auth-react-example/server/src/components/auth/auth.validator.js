392c55423b92b4543620ebd8f2efc2fcb824f5be
Duy <vietduy1512@gmail.com>
2020-03-22 00:42:29 +0700

    Update auth flow

--- auth-react-example/server/src/components/auth/auth.validator.js
+++ auth-react-example/server/src/components/auth/auth.validator.js
@@ -1,15 +1,14 @@
-const { body, validationResult, sanitizeBody } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 const User = require('../users/user.schema');
 
 exports.register = [
   body('email', 'Email is invalid.').isEmail(),
-  body('fullname', 'Fullname must not empty.').isLength({ min: 1 }).trim(),
+  body('fullname', 'Fullname must not empty.').isLength({ min: 1 }).trim().escape(),
   body('password', 'Password must at least 4 characters.').isLength({ min: 4 }),
-  body('confirmPassword', 'Confirm password is not matched.').equals('confirmPassword'),
-
-  sanitizeBody('fullname').trim().escape(),
+  body('confirmPassword', 'Confirm password is not matched.').custom((value, { req }) => value === req.body.password),
 
   async (req, res, next) => {
+    console.log(req.body)
     const errors = validationResult(req).array();
 
     let existedUser = await User.findOne({ email: req.body.email });
